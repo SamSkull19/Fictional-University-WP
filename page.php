95cf98ee8e790de42892a4b82b41224ef0472349
sifat-samin <sifatsamin19@gmail.com>
2025-11-07 17:21:11 +0600

    Global Header and Footer set

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+    get_header();
 
     while(have_posts()){
         the_post(); ?>
@@ -6,7 +7,8 @@
         <h1>This is a Page</h1>
         <h2><?php the_title(); ?></h2>
         <h3><?php the_content(); ?></h3>
+        
 <?php        
     }
-
+    get_footer();
 ?>

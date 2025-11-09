1c34b869b166f35b4fd405b90bf5dacd405d4585
sifat-samin <sifatsamin19@gmail.com>
2025-11-10 04:25:51 +0600

    Event Archive Page Added

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-                        <php><?php wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>"  class="nu gray">Learn more</a></p>
+                        <php><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>"  class="nu gray">Learn more</a></p>
                     </div>
                 </div>
 

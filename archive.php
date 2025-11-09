82bc9365c6f5818996823f8969a2fda6a04e094a
sifat-samin <sifatsamin19@gmail.com>
2025-11-09 20:21:13 +0600

    Archive page Title and Description added

--- archive.php
+++ archive.php
@@ -3,9 +3,21 @@
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome To Our Blog</h1>
+        <h1 class="page-banner__title">
+            <?php 
+                // if(is_category()){
+                //     single_cat_title();
+                // }
+
+                // if(is_author()){
+                //     echo 'Posted By '; the_author();
+                // }
+
+                the_archive_title();
+            ?>
+        </h1>
         <div class="page-banner__intro">
-            <p>Keep Up With Our Latest News.</p>
+            <p><?php the_archive_description(); ?></p>
         </div>
     </div>
 </div>

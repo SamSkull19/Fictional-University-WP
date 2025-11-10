e521be44464c815ac3b86bb9807063a54b28d3d3
sifat-samin <sifatsamin19@gmail.com>
2025-11-10 21:21:46 +0600

    Program Page Archive Added

---
+++ archive-program.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">
+            All Events
+        </h1>
+        <div class="page-banner__intro">
+            <p>Check Your Upcoming Events.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container container--narrow page-section">
+    <ul class="link-list min-list">
+        <?php
+        while (have_posts()) {
+            the_post(); ?>
+            <li><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        echo paginate_links();
+        ?>
+    </ul>
+
+    <hr class="section-break">
+    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check Out Past Events</a></p>
+</div>
+
+<?php get_footer(); ?>

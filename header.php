a3c844e9bdab75b108de696a840b96d204086a3b
sifat-samin <sifatsamin19@gmail.com>
2025-11-10 19:42:59 +0600

    Past Event Page Added

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 
                         <li <?php if(is_page('privacy-policy') or wp_get_post_parent_id(0) == 3) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/privacy-policy') ?>">Privacy Policy</a></li>
 
-                        <li <?php if(get_post_type() == 'event') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
+                        <li <?php if(get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
 
                         <li><a href="#">Campuses</a></li>
                         

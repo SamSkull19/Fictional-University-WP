7ac0898e2bf4855bc9d9dc9bdcbcc1fdda96a5c5
sifat-samin <sifatsamin19@gmail.com>
2025-11-13 23:31:16 +0600

    JS Search Option Started

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f5c1792359396246a4e7');
---
+++ src/modules/Search.js
@@ -0,0 +1,7 @@
+class Search {
+    constructor() {
+        alert("Hello JS");
+    }
+}
+
+export default Search;

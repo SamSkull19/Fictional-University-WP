70fcfefe93076dc0081c466d51b7d244ecb65445
sifat-samin <sifatsamin19@gmail.com>
2025-11-15 21:52:57 +0600

    Custom API Event relation With Program Query

--- inc/search_route.php
+++ inc/search_route.php
@@ -91,7 +91,7 @@
             }
 
             $programRelationshipQuery = new WP_Query(array(
-                'post_type' => 'professor',
+                'post_type' => array('professor', 'event'),
                 'meta_query' => $programsMetaQuery
             ));
 
@@ -102,12 +102,33 @@
                     array_push($results['professor'], array(
                         'title' => get_the_title(),
                         'permalink' => get_the_permalink(),
-                        'image' => get_the_post_thumbnail_url(0,    'professorLandscape')
+                        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                    ));
+                }
+
+                if(get_post_type() == 'event'){
+                    $eventDate = new DateTime(get_field('event_date'));
+                    $description = null;
+
+                    if (has_excerpt()) {
+                        $description = get_the_excerpt();
+                    } 
+                    else {
+                        $description = wp_trim_words(get_the_content(), 18);
+                    }
+
+                    array_push($results['event'], array(
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink(),
+                        'month' => $eventDate->format('M'),
+                        'day' => $eventDate->format('d'),
+                        'description' => $description
                     ));
                 }
             }
 
             $results['professor'] = array_values(array_unique($results['professor'], SORT_REGULAR));
+            $results['event'] = array_values(array_unique($results['event'], SORT_REGULAR));
         }
         
         return $results;

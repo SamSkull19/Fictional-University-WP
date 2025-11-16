63deaa548efb512697efeb479f9ca28fb5674a8d
sifat-samin <sifatsamin19@gmail.com>
2025-11-16 20:06:20 +0600

    Login Page Design Added

--- functions.php
+++ functions.php
@@ -126,3 +126,24 @@
             show_admin_bar(false);
         }
     }
+
+    add_filter('login_headerurl', 'ourHeaderUrl');
+
+    function ourHeaderUrl(){
+        return esc_url(site_url('/'));
+    }
+
+    add_action('login_enqueue_scripts', 'ourLoginCss');
+
+    function ourLoginCss(){
+        wp_enqueue_style('font_awesome', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+        wp_enqueue_style('google_font', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+        wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    }
+
+    add_filter('login_headertitle', 'ourLoginTitle');
+
+    function ourLoginTitle(){
+        return get_bloginfo('name');
+    }

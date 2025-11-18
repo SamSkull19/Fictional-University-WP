156ba4e4f1f6fdd7079b5e3445b74910cc2c1fa2
sifat-samin <sifatsamin19@gmail.com>
2025-11-18 19:59:22 +0600

    Security Increased

--- page-my-notes.php
+++ page-my-notes.php
@@ -41,7 +41,7 @@ while (have_posts()) {
 
                     <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
 
-                    <textarea readonly class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    <textarea readonly class="note-body-field"><?php echo esc_textarea(wp_strip_all_tags(get_the_content())); ?></textarea>
 
                     <span class="update-note btn btn--blue btr--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
                 </li>
